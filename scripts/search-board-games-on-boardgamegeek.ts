ba6321ec50380adbb00bad40fdb93d18eee62c8a
Mario De Luca <bloodcreed21@gmail.com>
2024-02-20 14:06:43 +0100

    modifica script board game geek

--- scripts/search-board-games-on-boardgamegeek.ts
+++ scripts/search-board-games-on-boardgamegeek.ts
@@ -1,11 +1,7 @@
 // Name: Search board games on BoardGameGeek
 
 import "@johnlindquist/kit"
-import * as cheerio from 'cheerio';
-
-const getBoardGames = async (query: string): Promise<GameInfo[]> => {
-    return (await get(`https://boardgamegeek.com/geeksearch.php?action=search&q=${encodeURIComponent(query)}&objecttype=boardgame`)).data.items as GameInfo[];
-};
+import { parseStringPromise } from "xml2js"
 
 type GameInfo = {
     href: string;
@@ -22,6 +18,31 @@ type GameInfo = {
 };
 
 
+const getBoardGames = async (query: string): Promise<GameInfo[]> => {
+    if (!query) return [];
+
+    const xmlString = (await get<string>(`https://boardgamegeek.com/xmlapi2/search?query=${encodeURIComponent(query)}&type=boardgame`)).data
+
+    const result = await parseStringPromise(xmlString, { trim: true }).catch(async error => {
+        await div(`Error parsing XML: ${error.message}`, "text-red-500")
+    })
+
+    const convertedGames = result['items']['item'].map(convertToGameInfo)
+
+    return convertedGames;
+};
+
+const convertToGameInfo = (item: any) => {
+    return {
+        id: item.$.id,
+        name: item.name[0].$.value,
+        yearpublished: item.yearpublished[0].$.value
+    }
+
+}
+
+
+
 const selectedGame = await arg("Search for a board game on BoardGameGeek", async (query) => {
     const games = await getBoardGames(query) || [];
     return games.map(game => ({
@@ -31,8 +52,7 @@ const selectedGame = await arg("Search for a board game on BoardGameGeek", async
 });
 
 await div(`
-    
-    <h1>${selectedGame.name} <span class="italic text-slate-500">(${selectedGame.yearpublished})</span></h1>
-    <a href="${selectedGame.href}">Click here for BoardGameGeek</a>
-
+    <div>
+        <h1>${selectedGame.name} <span class="italic text-slate-500">(${selectedGame.yearpublished})</span></h1>
+    </div>
 `, "p-2")
