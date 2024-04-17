1ec8a81f815504f129c5fd5615885fbf6a23654b
Mario De Luca <bloodcreed21@gmail.com>
2024-04-17 15:26:15 +0200

    refactor ai chat

--- scripts/ask-to-chatgpt.ts
+++ scripts/ask-to-chatgpt.ts
@@ -4,49 +4,29 @@
 // Group: AI
 
 import "@johnlindquist/kit";
-import { Configuration, CreateChatCompletionResponse, OpenAIApi } from 'openai-edge';
 
-type Model = {
-    name: string;
-    promptCost: number;
-    resultCost: number;
-}
 
-const models: Model[] = [
-    {
-        name: 'gpt-3.5-turbo' as const,
-        promptCost: 0.0005,
-        resultCost: 0.0015
-    },
-    {
-        name: 'gpt-4-0125-preview' as const,
-        promptCost: 0.01,
-        resultCost: 0.03
+const response = await fetch("https://api.anthropic.com/v1/messages", {
+    body: JSON.stringify({
+        model: "claude-3-haiku-20240307",
+        stream: false,
+        system: "You are an helpful AI, you will try to output concise and helpful answers to the user's questions, use markdown to format your answers",
+        temperature: 0.7,
+        max_tokens: 600,
+        messages: [
+            {
+                role: "user",
+                content: await arg("What do you want to ask?"),
+            }
+        ]
+    }),
+    method: "POST",
+    headers: {
+        "x-api-key": process.env["ANTHROPIC_API_KEY"],
+        "anthropic-version": "2023-06-01",
+        "Content-Type": "application/json"
     }
-];
-
-const config = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
 });
 
-const selectedModel = models[0]
-const openai = new OpenAIApi(config);
-const response = await openai.createChatCompletion({
-    model: selectedModel.name,
-    stream: false,
-    temperature: 1,
-    max_tokens: 600,
-    messages: [
-        {
-            role: "system",
-            content: "You are an helpful AI, you will try to output concise and helpful answers to the user's questions, use markdown to format your answers"
-        },
-        {
-            role: "user",
-            content: await arg("What do you want to ask?"),
-        }
-    ]
-});
-
-const data: CreateChatCompletionResponse = await response.json();
-await div(md(`${data.choices[0].message.content}`));
+const data = (await response.json()).content[0].text;
+await div(md(`${data}`));
--- scripts/syncronize-obsidian.ts
+++ scripts/syncronize-obsidian.ts
@@ -6,15 +6,27 @@ import { simpleGit, SimpleGit } from 'simple-git';
 const OBSIDIAN_VAULT_PATH = "C:/Users/blood/Desktop/Progetti/ObsidianNotes"
 const git: SimpleGit = simpleGit(OBSIDIAN_VAULT_PATH);
 
+const listChangedFiles = (fileName: string) => {
+    return `<p>${fileName} changes</p>`;
+
+}
+
 div("Syncronizing Obsidian vault with git...", "p-2 text-center")
 const gitpullResponse = await git.pull()
 await div(md(`${gitpullResponse.summary.insertions.toString()} insertions, ${gitpullResponse.summary.deletions.toString()} deletions.
 
-${gitpullResponse.files.join("\n")}
+${gitpullResponse.files.map(listChangedFiles)}
 `))
 
 const gitStatus = await git.status();
 if (gitStatus.files.length > 0) {
+
+    await div(`
+    <p>Changes to commit:</p>
+    <ul class="p-2">
+        ${gitStatus.files.map(file => `<li>${file.path}</li>`).join("")}
+    </ul>
+    `)
     const commitMessage = await arg("Enter commit message:");
     await git.add(".");
     await git.commit(commitMessage);
